55773083e284a8e09689c48b57f6650ed53a121a
danda <dan-da@users.noreply.github.com>
2020-11-16 14:34:25 -0800

    attempt to make it work with sb_net_mem crate.  work in progress, test does not build

--- src/sb_orswot.rs
+++ src/sb_orswot.rs
@@ -94,3 +94,64 @@ impl<M: Clone + Eq + std::hash::Hash + std::fmt::Debug + Serialize> SecureBroadc
         self.orswot.apply(op);
     }
 }
+
+/*
+impl SecureBroadcastAlgorithm
+    for SBOrswot<u8>
+{
+    type Op = orswot::Op<u8, Actor>;
+    type ReplicatedState = orswot::Orswot<u8, Actor>;
+
+    fn new(actor: Actor) -> Self {
+        Self {
+            actor,
+            orswot: orswot::Orswot::new(),
+        }
+    }
+
+    fn state(&self) -> Self::ReplicatedState {
+        self.orswot.clone()
+    }
+
+    fn sync_from(&mut self, other: Self::ReplicatedState) {
+        self.orswot.merge(other);
+    }
+
+    fn validate(&self, from: &Actor, op: &Self::Op) -> bool {
+        let validation_tests = match op {
+            orswot::Op::Add { dot, members: _ } => vec![
+                (
+                    &dot.actor == from,
+                    "Attempting to add with a dot different from the source proc",
+                ),
+                (
+                    dot == &self.orswot.clock().inc(*from),
+                    "Dot is not a direct successor",
+                ),
+            ],
+            orswot::Op::Rm { clock, members } => vec![
+                (
+                    members.len() == 1,
+                    "We only support removes of a single element",
+                ),
+                (
+                    // NOTE: this check renders all the "deferred_remove" logic in the ORSWOT obsolete.
+                    //       The deferred removes would buffer these out-of-order removes.
+                    clock <= &self.orswot.clock(),
+                    "This rm op is removing data we have not yet seen",
+                ),
+            ],
+        };
+
+        validation_tests
+            .into_iter()
+            .find(|(is_valid, _msg)| !is_valid)
+            .map(|(_test, msg)| println!("[ORSWOT/VALIDATION] {} {:?}, {:?}", msg, op, self))
+            .is_none()
+    }
+
+    fn apply(&mut self, op: Self::Op) {
+        self.orswot.apply(op);
+    }
+}
+*/
